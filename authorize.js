7bef6284a8282b4a8c5a1c465b1ef0c0917673ca
andranik-sargsyan <csharp.and@hotmail>
2023-06-30 06:31:36 +0400

    Add project files.

---
+++ authorize.js
@@ -0,0 +1,9 @@
+﻿let session = localStorage["session"];
+
+session = session ? JSON.parse(session) : {};
+
+localStorage["session"] = JSON.stringify(session);
+
+if (!session.user) {
+    location.href = "login.html";
+}
---
+++ login.js
@@ -0,0 +1,42 @@
+﻿let btnLogin = document.getElementById("btn-login");
+
+let users = [
+    {
+        id: 1,
+        name: "Admin Adminyan",
+        email: "admin@test.com",
+        password: "admin123!",
+        role: "admin"
+    },
+    {
+        id: 2,
+        name: "User Useryan",
+        email: "user@test.com",
+        password: "user123!",
+        role: "user"
+    }
+];
+
+btnLogin.addEventListener("click", () => {
+    let txtEmail = document.getElementById("txt-email");
+    let txtPassword = document.getElementById("txt-password");
+
+    let user = users.find(u => u.email == txtEmail.value && u.password == txtPassword.value);
+
+    if (user) {
+        localStorage["session"] = JSON.stringify({ user: user });
+
+        if (user.role == "admin") {
+            location.href = "admin.html";
+        }
+        else if (user.role == "user") {
+            location.href = "user.html";
+        }
+        else {
+            alert("Invalid role.");
+        }
+    }
+    else {
+        alert("Invalid user.");
+    }
+});
